afd2ae7018a57f0effaf3ba17a10e2e18b2f1127
Valentin Allmnd <valentin.allmnd@gmail.com>
2025-03-21 10:34:08 +0100

    refactor parsing part

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod tls;
 use tls::utils::{configure_server_tls,extract_client_certificate};
-
+mod parsing;
+use parsing::utils::load_and_parse_json;
 use hyper::StatusCode;
 
 use serde_json::{from_reader, json, to_writer_pretty, Value};
@@ -12,20 +13,9 @@ use std::path::Path;
 use tokio::net::TcpListener;
 use std::fs::{self, File, OpenOptions};
 use std::io::{BufReader, BufWriter};
-use urlencoding::encode;
-use serde::{Serialize, Deserialize};
 use wireguard_keys::Privkey;
 
-#[derive(Serialize, Deserialize, Debug,Clone)]
-pub struct ClientData {
-    address: String,
-    port: String,
-    privkey: String,
-    pubkey: String,
-    allowedip: String,
-    allowedmask: String, 
-}
-type ClientMap = HashMap<String, ClientData>;
+
 
 
 #[tokio::main]
@@ -230,76 +220,7 @@ async fn main() {
 
 
 
-async fn load_and_parse_json(file_path: &str, id_client_x_value: &str) -> (StatusCode, String) {
-    let file_result = async_fs::read_to_string(file_path).await;
 
-    match file_result {
-        Ok(contents) => {
-            match parse_client_json(&contents, id_client_x_value) {
-                Ok(client_data) => {
-                    println!("Config trouvée : {:?}", client_data);
-                    let encoded_data = create_urlencoded_data(&client_data);
-                    //println!("Encoded: {}", encoded_data);
-                    return (StatusCode::OK, encoded_data);
-                }
-                Err(e) => {
-                    println!("Erreur lors du parsing : {}", e);
-                    return (
-                        StatusCode::SERVICE_UNAVAILABLE,
-                        "Cannot send you the config".to_string(),
-                    );
-                }
-            }
-        }
-        Err(e) => {
-            println!("Erreur lors de la récupération du contenu du fichier : {}", e);
-            return (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                "Cannot send you the config".to_string(),
-            );
-        }
-    }
-}
-
-pub fn parse_client_json(json_str: &str, client_id: &str) -> Result<ClientData, String> {
-    let clients: ClientMap = match serde_json::from_str(json_str) {
-        Ok(c) => c,
-        Err(e) => return Err(format!("Erreur de format JSON : {}", e)),
-    };
-    match clients.get(client_id) {
-        Some(client_data) => Ok(client_data.clone()), // Clone pour retourner une copie de ClientData
-        None => Err(format!("Client ID '{}' non trouvé", client_id)),
-    }
-}
-
-pub fn create_urlencoded_data(client_data: &ClientData) -> String {
-    let mut data = HashMap::new();
-    
-    data.insert("address", &client_data.address);
-    data.insert("port", &client_data.port);
-    data.insert("privkey", &client_data.privkey);
-    data.insert("public_key_server", &client_data.pubkey);
-    data.insert("allowedip", &client_data.allowedip);
-    data.insert("allowedmask", &client_data.allowedmask);
-
-    let ordered_fields = vec![
-        ("address", &client_data.address),
-        ("port", &client_data.port),
-        ("privkey", &client_data.privkey),
-        ("pubkey", &client_data.pubkey),
-        ("allowedip", &client_data.allowedip),
-        ("allowedmask", &client_data.allowedmask),
-    ];
-
-    let encoded_data: String = ordered_fields
-        .iter()
-        .map(|(key, value)| format!("{}={}", encode(key), encode(value)))
-        .collect::<Vec<String>>()
-        .join("&");
-
-
-    encoded_data
-}
 
 /// Extract the path from an HTTP request.
 ///
---
+++ src/parsing/utils.rs
@@ -0,0 +1,87 @@
+use std::collections::HashMap;
+
+use hyper::StatusCode;
+use serde::{Serialize, Deserialize};
+use urlencoding::encode;
+
+#[derive(Serialize, Deserialize, Debug,Clone)]
+pub struct ClientData {
+    address: String,
+    port: String,
+    privkey: String,
+    pubkey: String,
+    allowedip: String,
+    allowedmask: String, 
+}
+type ClientMap = HashMap<String, ClientData>;
+
+pub async fn load_and_parse_json(file_path: &str, id_client_x_value: &str) -> (StatusCode, String) {
+    let file_result = async_fs::read_to_string(file_path).await;
+
+    match file_result {
+        Ok(contents) => {
+            match parse_client_json(&contents, id_client_x_value) {
+                Ok(client_data) => {
+                    println!("Config trouvée : {:?}", client_data);
+                    let encoded_data = create_urlencoded_data(&client_data);
+                    //println!("Encoded: {}", encoded_data);
+                    return (StatusCode::OK, encoded_data);
+                }
+                Err(e) => {
+                    println!("Erreur lors du parsing : {}", e);
+                    return (
+                        StatusCode::SERVICE_UNAVAILABLE,
+                        "Cannot send you the config".to_string(),
+                    );
+                }
+            }
+        }
+        Err(e) => {
+            println!("Erreur lors de la récupération du contenu du fichier : {}", e);
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Cannot send you the config".to_string(),
+            );
+        }
+    }
+}
+
+pub fn parse_client_json(json_str: &str, client_id: &str) -> Result<ClientData, String> {
+    let clients: ClientMap = match serde_json::from_str(json_str) {
+        Ok(c) => c,
+        Err(e) => return Err(format!("Erreur de format JSON : {}", e)),
+    };
+    match clients.get(client_id) {
+        Some(client_data) => Ok(client_data.clone()), // Clone pour retourner une copie de ClientData
+        None => Err(format!("Client ID '{}' non trouvé", client_id)),
+    }
+}
+
+pub fn create_urlencoded_data(client_data: &ClientData) -> String {
+    let mut data = HashMap::new();
+    
+    data.insert("address", &client_data.address);
+    data.insert("port", &client_data.port);
+    data.insert("privkey", &client_data.privkey);
+    data.insert("public_key_server", &client_data.pubkey);
+    data.insert("allowedip", &client_data.allowedip);
+    data.insert("allowedmask", &client_data.allowedmask);
+
+    let ordered_fields = vec![
+        ("address", &client_data.address),
+        ("port", &client_data.port),
+        ("privkey", &client_data.privkey),
+        ("pubkey", &client_data.pubkey),
+        ("allowedip", &client_data.allowedip),
+        ("allowedmask", &client_data.allowedmask),
+    ];
+
+    let encoded_data: String = ordered_fields
+        .iter()
+        .map(|(key, value)| format!("{}={}", encode(key), encode(value)))
+        .collect::<Vec<String>>()
+        .join("&");
+
+
+    encoded_data
+}
